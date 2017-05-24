9886c1e29a3a0506c7b356db31d4889716ec15c6
Julian Descottes <julian.descottes@gmail.com>
2017-05-24 10:14:58 +0200

    [GITHUB] remove -q flag for git fetch artifacts

--- bin/checkout-artifacts.sh
+++ bin/checkout-artifacts.sh
@@ -15,13 +15,24 @@ if [ -d $ARTIFACTS_DIR ]; then
   fi
 fi
 
-if [ ! -d $ARTIFACTS_DIR/.git ]; then 
+if [ ! -d $ARTIFACTS_DIR/.git ]; then
   rm -rf $ARTIFACTS_DIR
   git init $ARTIFACTS_DIR
 fi
 
+ARTIFACTS_LOCATION="https://github.com/ochameau/ff-dt.git artifacts-$BRANCH"
+
+echo "Now downloading test dependencies via git ($ARTIFACTS_LOCATION)."
+
+if [ $CI ]; then
+  # Silence logs when running on CI.
+  QUIET=-q
+else
+  QUIET=
+fi
+
 pushd $ARTIFACTS_DIR > /dev/null
-git fetch https://github.com/ochameau/ff-dt.git artifacts-$BRANCH -q
+git fetch $ARTIFACTS_LOCATION $QUIET
 git checkout $SHA
 popd > /dev/null
 
